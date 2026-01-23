9bda92c10f893262ee48934f2ae7a6c411c04540
nsubaru11 <20051126subaru@gmail.com>
2026-01-23 13:45:57 +0900

    chore: update to v1.0.1, simplify auto-login conditions and clean up redundant comments

--- src/wakayama-u-moodle-autologin.user.js
+++ src/wakayama-u-moodle-autologin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wakayama University Moodle Auto-Login
 // @namespace    https://github.com/nsubaru11/userscripts
-// @version      1.0.0
+// @version      1.0.1
 // @description  和歌山大学の moodle に自動でログインを行います。
 // @author       nsubaru11
 // @license      MIT
@@ -25,7 +25,6 @@
 	if (currentUrl.includes('wakayama-u.ac.jp')) {
 
 		// A. ログインページ（index.php または index_form.html）での処理
-		// 修正点: index_form.html も条件に追加
 		if (currentUrl.includes('login/index.php') || currentUrl.includes('login/index_form.html')) {
 
 			// Microsoft 365 / OIDC ログインボタンを探してクリック
@@ -34,19 +33,15 @@
 
 			if (oidcButton) {
 				// 誤作動防止のためごく短い遅延を入れる
-				setTimeout(() => {
-					oidcButton.click();
-				}, 300);
-				return; // 処理終了
+				setTimeout(() => oidcButton.click(), 300);
+				return;
 			}
 		}
 
 		// B. トップページ等で「ログイン」リンクが表示されている場合
 		// ログインページに既にいる場合は実行しない
 		const loginLink = document.querySelector('.logininfo a[href*="login/index.php"], .login a[href*="login/index.php"]');
-		if (loginLink && !currentUrl.includes('login/')) {
-			loginLink.click();
-		}
+		if (loginLink && !currentUrl.includes('login/')) loginLink.click();
 	}
 
 	// 2. Microsoft ログインページでの処理
