5b67a12c92162ccefe6ba0e1377afe418cb4716d
nsubaru11 <20051126subaru@gmail.com>
2026-04-06 15:34:41 +0900

    feat: bump version to 1.1.1 for Enhanced CampusSquare Syllabus script

--- src/enhanced-campus-square-syllabus.user.js
+++ src/enhanced-campus-square-syllabus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Enhanced CampusSquare Syllabus
 // @namespace    https://github.com/nsubaru11/userscripts
-// @version      1.1.0
+// @version      1.1.1
 // @description  検索に便利な週間カレンダーを表示します。
 // @author       nsubaru11
 // @license      MIT
